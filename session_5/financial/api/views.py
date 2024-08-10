02c1fd7c6693e6e061483b0ca3c5cdc3055cdfbf
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2024-08-10 08:21:54 +0330

    Make Serializer For Views.py And Permissions

--- session_5/financial/api/views.py
+++ session_5/financial/api/views.py
@@ -1,27 +1,47 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from api.serializers import AccountSerializer, CategorySerializer, TransactionSerializer
 from main.models import Category, Account, Transaction
 
 
 class AccountAPIView(APIView):
+    """
+        /api/accounts:
+            sdfsdfsdfsdfgsfdg
+    """
+    serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
         accounts = Account.objects.all()
-        serialized_data = AccountSerializer(accounts, many=True)
+        serialized_data = self.serializer_class(accounts, many=True)
         return Response(serialized_data.data)
 
 
 class CategoryAPIView(APIView):
+    """
+        /api/category:
+            sdfsdfsdfsdfgsfdg
+    """
+    serializer_class = CategorySerializer
+
     def get(self, request):
-        accounts = Category.objects.all()
-        serialized_data = CategorySerializer(accounts, many=True)
+        categories = Category.objects.all()
+        serialized_data = self.serializer_class(categories, many=True)
         return Response(serialized_data.data)
 
 
 class TransactionAPIView(APIView):
+    """
+        /api/transactions:
+            sdfsdfsdfsdfgsfdg
+    """
+    serializer_class = TransactionSerializer
+
     def get(self, request):
-        accounts = Transaction.objects.all()
-        serialized_data = TransactionSerializer(accounts, many=True)
+        transactions = Transaction.objects.filter(account__bank_name="ملت")
+        serialized_data = self.serializer_class(transactions, many=True)
         return Response(serialized_data.data)
